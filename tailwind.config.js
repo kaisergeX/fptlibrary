ae9d041dcbbb2a0c260d7a58b4c7871694088da3
Duc Khai <khaivd98@gmail.com>
2023-10-28 21:02:48 +0700

    chore: manually reset styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,7 @@ import defaultTheme from 'tailwindcss/defaultTheme';
 
 /** @type {import('tailwindcss').Config} */
 export default {
+  corePlugins: {preflight: false},
   content: ['./index.html', './src/**/*.{ts,tsx}'],
   theme: {
     extend: {

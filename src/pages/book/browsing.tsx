acfd2b6c1c052d8ef24dda249159c91848764767
Duc Khai <khaivd98@gmail.com>
2023-12-06 22:41:38 +0700

    feat: book browsing filter

--- src/pages/book/browsing.tsx
+++ src/pages/book/browsing.tsx
@@ -1,20 +1,38 @@
-import {ActionIcon} from '@mantine/core';
+import {ActionIcon, MultiSelect, Select} from '@mantine/core';
+import {useForm, zodResolver} from '@mantine/form';
 import {useLocalStorage} from '@mantine/hooks';
 import {IconLayoutColumns, IconLayoutRows} from '@tabler/icons-react';
 import {useQuery} from '@tanstack/react-query';
 import {useTranslation} from 'react-i18next';
 import {generatePath, useNavigate} from 'react-router-dom';
+import {z} from 'zod';
 import BookCard from '~/components/book/card';
+import i18n from '~/config/i18n';
 import {Path} from '~/config/path';
+import {MAX_GENRES} from '~/config/system';
+import {BookStatus} from '~/constants';
 import {API, QueryKey} from '~/constants/service';
+import useMasterData from '~/hook/useMasterData';
 import {Head} from '~/layout/outlet/Head';
-import type {Book, ResponseData} from '~/types';
+import type {Book, BookFilterFormValues, BookFormValues, ResponseData} from '~/types';
 import {classNames} from '~/util';
 import {http} from '~/util/http';
+import {zodCustomErrorMap} from '~/util/validation';
+
+const validationSchema: z.ZodSchema<Partial<BookFilterFormValues>> = z.object({
+  title: z.string({errorMap: zodCustomErrorMap}).trim().optional(),
+  publishYear: z.date().nullish(),
+  genre: z.array(z.string({errorMap: zodCustomErrorMap})).optional(),
+  country: z.array(z.string({errorMap: zodCustomErrorMap})).optional(),
+  ageTag: z.array(z.string({errorMap: zodCustomErrorMap})).optional(),
+  status: z.nativeEnum(BookStatus).optional(),
+});
 
 export default function BookBrowsing() {
   const {t} = useTranslation();
   const navigate = useNavigate();
+  const {selectCountryList, selectAgeTagList, selectGenreList} = useMasterData();
+
   const [horizontalItem, setHorizontalItem] = useLocalStorage({
     key: 'book-browsing-layout',
     defaultValue: false,
@@ -22,7 +40,8 @@ export default function BookBrowsing() {
 
   const {data: renderBooks} = useQuery({
     queryKey: [QueryKey.BOOKS],
-    queryFn: () => http.get<ResponseData<Book[]>>(API.BOOKS),
+    queryFn: () => http.get<ResponseData<Book[]>>('/dump/books.json', {baseURL: '/'}),
+    // queryFn: () => http.get<ResponseData<Book[]>>(API.BOOKS),
     select: ({body: bookData}) =>
       bookData.map((data) => (
         <BookCard
@@ -33,6 +52,25 @@ export default function BookBrowsing() {
           hozizontal={horizontalItem}
         />
       )),
+  });
+
+  const {
+    values: formValues,
+    getInputProps,
+    setFieldValue,
+    setInitialValues,
+    onSubmit,
+    isDirty,
+    reset,
+  } = useForm<z.infer<typeof validationSchema>>({
+    initialValues: {
+      title: '',
+      genre: [],
+      country: [],
+      ageTag: [],
+    },
+    validate: zodResolver(validationSchema),
+    validateInputOnChange: true,
   });
 
   return (
@@ -55,6 +93,36 @@ export default function BookBrowsing() {
               <IconLayoutColumns size="1.5rem" />
             )}
           </ActionIcon>
+        </div>
+
+        <div className="flex items-center gap-4">
+          <Select
+            label={t('common.country')}
+            data={selectCountryList}
+            searchable
+            allowDeselect={false}
+            checkIconPosition="right"
+            {...getInputProps('country')}
+          />
+          <MultiSelect
+            label={t('ageTag.def')}
+            description={t('ageTag.select')}
+            data={selectAgeTagList}
+            checkIconPosition="right"
+            hidePickedOptions
+            {...getInputProps('ageTag')}
+          />
+          <MultiSelect
+            label={t('genre.def')}
+            description={t('common.maxOptions', {max: MAX_GENRES})}
+            data={selectGenreList}
+            searchable
+            checkIconPosition="right"
+            maxValues={MAX_GENRES}
+            clearable
+            hidePickedOptions
+            {...getInputProps('genre')}
+          />
         </div>
 
         <div

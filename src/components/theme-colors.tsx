fed1f7c180241560e392edff20441fa453e3730f
Duc Khai <khaivd98@gmail.com>
2023-12-21 20:39:51 +0700

    chore: remove `dark` theme on dark mode & `gray` theme on light mode

--- src/components/theme-colors.tsx
+++ src/components/theme-colors.tsx
@@ -4,6 +4,7 @@ import {
   Tooltip,
   useMantineTheme,
   type ColorSwatchProps,
+  useMantineColorScheme,
 } from '@mantine/core';
 import {usePersistStore} from '~/store';
 import {classNames} from '~/util';
@@ -17,21 +18,33 @@ const ThemeColors = ({
 }) => {
   const {theme, setTheme} = usePersistStore();
   const {colors} = useMantineTheme();
+  const {colorScheme} = useMantineColorScheme();
+  const isDarkMode = colorScheme === 'dark';
 
-  const swatches = Object.keys(colors).map((color) => (
-    <Tooltip label={color} key={color}>
-      <ColorSwatch
-        className="cursor-pointer text-white opacity-80 hover:opacity-100 hover:shadow-lg disabled:cursor-default"
-        component="button"
-        color={colors[color][6]}
-        size={size}
-        onClick={() => setTheme(color)}
-        disabled={theme === color}
-      >
-        {theme === color && <CheckIcon size={16} />}
-      </ColorSwatch>
-    </Tooltip>
-  ));
+  const swatches = Object.keys(colors).flatMap((color) => {
+    if (isDarkMode && color === 'dark') {
+      return [];
+    }
+
+    if (!isDarkMode && color === 'gray') {
+      return [];
+    }
+
+    return (
+      <Tooltip label={color} key={color}>
+        <ColorSwatch
+          className="cursor-pointer text-white opacity-80 hover:opacity-100 hover:shadow-lg disabled:cursor-default"
+          component="button"
+          color={colors[color][6]}
+          size={size}
+          onClick={() => setTheme(color)}
+          disabled={theme === color}
+        >
+          {theme === color && <CheckIcon size={16} />}
+        </ColorSwatch>
+      </Tooltip>
+    );
+  });
 
   return <div className={classNames('my-4 flex gap-2', className)}>{swatches}</div>;
 };
